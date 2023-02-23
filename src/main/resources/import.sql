8c41144c538488d50b2d670b074b8c1649ff8f38
Jeezyel P Costa <72239179+Jeezyel@users.noreply.github.com>
2023-02-23 20:29:54 -0300

    ajustes para rodar

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,8 @@
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+
+
+
+
+-- insert into Roupas (id,quantidade,valor,cor,tipoDeTercido,marca ) values(nextval('hibernate_sequence'), 6 , 84 , 'branco','algodão','lacost');
