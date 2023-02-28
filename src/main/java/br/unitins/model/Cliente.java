bc301aca7cc7de8fbfabb77965a7f627df2e9e42
Jeezyel P Costa <72239179+Jeezyel@users.noreply.github.com>
2023-02-28 08:24:59 -0300

    ajuste e get,set

--- src/main/java/br/unitins/model/Cliente.java
+++ src/main/java/br/unitins/model/Cliente.java
@@ -2,15 +2,41 @@ package br.unitins.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Cliente {
+
+    @Column(nullable = false, length = 80)
     private String nome;
+    @Id    //usando o cpf como chave Primaria 
     private String cpf;
-    @Id
+    
     private LocalDate dataDeNacimento;
+
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public LocalDate getDataDeNacimento() {
+        return dataDeNacimento;
+    }
+    public void setDataDeNacimento(LocalDate dataDeNacimento) {
+        this.dataDeNacimento = dataDeNacimento;
+    }
+
+    
     
 
 }
