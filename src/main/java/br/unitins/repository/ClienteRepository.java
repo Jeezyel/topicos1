9aed3554dec7cf3a8e53a3c08525757ae468c336
Jeezyel P Costa <72239179+Jeezyel@users.noreply.github.com>
2023-03-18 13:51:38 -0300

    implementando o Repository co Cliente

--- src/main/java/br/unitins/repository/ClienteRepository.java
+++ src/main/java/br/unitins/repository/ClienteRepository.java
@@ -1,5 +1,31 @@
 package br.unitins.repository;
 
-public class ClienteRepository {
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.jboss.tm.FirstResource;
+
+import br.unitins.model.Cliente;
+import io.netty.buffer.PoolChunkListMetric;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+
+@ApplicationScoped
+public class ClienteRepository implements PanacheRepository {
+
+    public Cliente findByNome(String nome){
+        if(nome == null){
+            return null;
+        }
+        return (Cliente) find("nome",  nome  ).firstResult();
+    }
+
+    public List<Cliente> findByNomeList(String nome){
+        if(nome == null){
+            return null;
+        }
+        return find("UPPER(nome) LIKE ?1 ", "%"+ nome.toUpperCase() +"%").list();
+    }
     
 }
