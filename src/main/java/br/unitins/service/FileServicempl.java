cbb2afbc4df57df6a9a9cdff81a114ac9e791b21
Jeezyel P Costa <jeezyelpereira@gmail.com>
2023-05-20 09:41:05 -0300

    .

--- src/main/java/br/unitins/service/FileServicempl.java
+++ src/main/java/br/unitins/service/FileServicempl.java
@@ -1,20 +1,70 @@
 package br.unitins.service;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 public class FileServicempl implements FileService{
+    ///                                                                      isso e isso / ou \
+    private final String PATH_USER = System.getProperty("user.home") + File.separator + "quarkus" + File.separator + "images" + File.separator + "usuaria" + File.separator ;
 
     @Override
     public String salvarImagemUsuario(byte[] imagem, String nomeImagem) throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'salvarImagemUsuario'");
+
+        //verificar o tipo da imagem
+        String mineType = Files.probeContentType(new File(nomeImagem).toPath());
+
+        List<String> listMineType = Arrays.asList("image/jpg","image/png","image/gif");
+
+        //alerta de tipo de images
+        if (!listMineType.contains(mineType)) {
+            throw new IOException("imagem na suportada");
+        }
+
+        //alerta de tamanho 
+        if (imagem.length > (1024 * 1024 *10)) {
+            throw new IOException("tamanha não suportado ");
+        }
+
+        //verifica se tem a pasta 
+        File diretorio = new File(PATH_USER);
+        if (!diretorio.exists()) {
+            diretorio.mkdirs();
+        }
+
+        // gerando o nome do arquivo 
+        String nomeArquivo = UUID.randomUUID() +"." +mineType.substring(mineType.lastIndexOf("/")+1);
+
+        String path = PATH_USER + nomeArquivo;
+
+        //salvar
+        File file = new File(path);
+        if (file.exists()) {
+            throw new IOException("o nome gerado da imagem esta sendo usado");
+        }
+
+        //criando um arquivo no S.O.
+        file.createNewFile();
+
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(imagem);
+
+        //garnatindo que mando o ultimo lote de byte
+        fos.flush();
+        fos.close();
+
+        return nomeArquivo;
     }
 
     @Override
     public File download(String nomeArquivo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'download'");
+        File file = new File(nomeArquivo);
+
+        return file;
     }
     
 }
