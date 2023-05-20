7f5fdd41af548a508de966d042306bdb8b895eab
Jeezyel P Costa <jeezyelpereira@gmail.com>
2023-05-20 08:48:01 -0300

    metodos a ser implementados

---
+++ src/main/java/br/unitins/service/FileService.java
@@ -0,0 +1,12 @@
+package br.unitins.service;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface FileService {
+
+    String salvarImagemUsuario(byte[] imagem, String nomeImagem) throws IOException;
+
+    File download(String nomeArquivo);
+    
+}
