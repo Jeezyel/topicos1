0d3b6d21ad69b90acca6c1e82c1fbb77f2980eda
Jeezyel P Costa <jeezyelpereira@gmail.com>
2023-05-20 08:48:40 -0300

    .

--- src/main/java/br/unitins/service/FileServicempl.java
+++ src/main/java/br/unitins/service/FileServicempl.java
@@ -1,5 +1,20 @@
 package br.unitins.service;
 
-public class FileServicempl {
+import java.io.File;
+import java.io.IOException;
+
+public class FileServicempl implements FileService{
+
+    @Override
+    public String salvarImagemUsuario(byte[] imagem, String nomeImagem) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'salvarImagemUsuario'");
+    }
+
+    @Override
+    public File download(String nomeArquivo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'download'");
+    }
     
 }
