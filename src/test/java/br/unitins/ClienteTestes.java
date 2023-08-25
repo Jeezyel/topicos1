b8234cce54287945a784d32a3b708b9511a22b3b
Jeezyel P Costa <72239179+Jeezyel@users.noreply.github.com>
2023-08-24 23:15:01 -0300

    mechendo nos teste

--- src/test/java/br/unitins/ClienteTestes.java
+++ src/test/java/br/unitins/ClienteTestes.java
@@ -1,9 +1,5 @@
 package br.unitins;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import br.unitins.service.ClienteService;
--- src/test/java/br/unitins/RoupaTestes.java
+++ src/test/java/br/unitins/RoupaTestes.java
@@ -1,10 +1,6 @@
 package br.unitins;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
